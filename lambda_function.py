4b739425276a01505314588f2976c318c16a5003
XiaozheCheng <117330000+XiaozheCheng@users.noreply.github.com>
2023-04-05 19:43:08 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,17 @@
+import csv
 
-import json
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+  
+    key = '721p4.csv'
+    
+    response = s3.get_object(Bucket='my-bucket', Key=key)
+    content = response['Body'].read().decode('utf-8')
+    
+    lines = content.split('\n')
+    for line in csv.reader(lines):
+        print(line)
 
-    name = event['name']
-    age = event['age']
 
- 
-    message = f"Hello, {name}! You are {age} years old."
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(message)
-    }
 
